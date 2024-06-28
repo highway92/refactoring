26fd9ae61840bf61b0b64393cb91dc07708a4243
highway92 <highway92@kakao.com>
2024-06-28 14:44:29 +0900

    format 함수 naming 변경

--- function statement/statement.js
+++ function statement/statement.js
@@ -2,11 +2,14 @@ export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명 ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   for (let perf of invoice.performances) {
     function playFor(aPerformance) {
@@ -47,11 +50,11 @@ export function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액 ${format(totalAmount / 100)}\n`;
+  result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
 }
