90cff70dc99f2d7130d850d9e44b413066b20ea3
highway92 <highway92@kakao.com>
2024-06-28 14:00:31 +0900

    switch 문 함수 추출하기

--- function statement/statement.js
+++ function statement/statement.js
@@ -10,25 +10,31 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 30);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
+    function amountFor(perf, play) {
+      let thisAmount = 0;
+
+      switch (play.type) {
+        case "tragedy":
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case "comedy":
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 30);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르 : ${play.type}`);
+      }
+      return thisAmount;
     }
+
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     //희극 관객 5명마다 추가 포인트를 제공한다.
