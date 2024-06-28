34859c3daaf3be6c0897b15c933e68a4b51f9e91
highway92 <highway92@kakao.com>
2024-06-28 16:57:20 +0900

    test code 업데이트 및 함수 쪼개기

--- function statement/statement.js
+++ function statement/statement.js
@@ -1,4 +1,13 @@
-export function renderPlainText(data, plays) {
+import { createStatementData } from "./createStatementData";
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+function renderPlainText(data) {
   let result = `청구 내역 (고객명 ${data.customer})\n`;
 
   for (let perf of data.performances) {
@@ -9,75 +18,26 @@ export function renderPlainText(data, plays) {
   result += `총액 ${usd(data.totalAmount)}\n`;
   result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>총액 : <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
 }
 
 export function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
-export function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 30);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
-    return result;
-  }
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-  return statementData;
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
 }
--- function statement/statement.test.js
+++ function statement/statement.test.js
@@ -1,4 +1,4 @@
-import { statement } from "./statement";
+import { statement, htmlStatement } from "./statement";
 import invoices from "./invoices.json" with { type: "json" };
 import plays from "./plays.json" with { type: "json" };
 
@@ -11,4 +11,18 @@ test("statement func 테스트", () => {
       "총액 $1,760.00\n" +
       "적립 포인트 : 55점\n"
   );
+});
+
+test("htmlStatement func 테스트", () => {
+  const expectedHTML = `<h1>청구 내역 (고객명: BigCo)</h1>
+<table>
+<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr> <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>
+ <tr><td>As You Like It</td><td>(35석)</td><td>$530.00</td></tr>
+ <tr><td>Othello</td><td>(48석)</td><td>$580.00</td></tr>
+</table>
+<p>총액 : <em>$1,760.00</em></p>
+<p>적립 포인트: <em>55</em>점</p>
+`;
+
+  expect(htmlStatement(invoices[0], plays)).toBe(expectedHTML);
 });
