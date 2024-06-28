fd2e84c0fd4b6229dfaaa49938cc165a5e508307
highway92 <highway92@kakao.com>
2024-06-28 14:52:43 +0900

    totalVolume refactoriing

--- function statement/statement.js
+++ function statement/statement.js
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명 ${invoice.customer})\n`;
 
   function usd(aNumber) {
@@ -10,51 +9,55 @@ export function statement(invoice, plays) {
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 30);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      result += Math.floor(perf.audience / 5);
+    return result;
+  }
 
   for (let perf of invoice.performances) {
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-      let result = 0;
-
-      switch (playFor(aPerformance).type) {
-        case "tragedy":
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 30);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
-      }
-      return result;
-    }
-
-    function volumeCreditsFor(perf) {
-      let result = 0;
-      result += Math.max(perf.audience - 30, 0);
-      if ("comedy" === playFor(perf).type)
-        result += Math.floor(perf.audience / 5);
-      return result;
-    }
-
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
 
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
   result += `총액 ${usd(totalAmount)}\n`;
-  result += `적립 포인트 : ${volumeCredits}점\n`;
+  result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
   return result;
 }
